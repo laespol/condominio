12ab04e74849f199f720a15aede4593a3491e55c
Luis Espoladore <laespol@sistema>
2016-12-07 16:09:15 -0200

    teste jenkins 3

---
+++ src/test/java/br/com/adprofissionais/dao/TipoContatoDAOTest.java
@@ -0,0 +1,34 @@
+package br.com.adprofissionais.dao;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import br.com.adprofissionais.domain.TipoContato;
+
+public class TipoContatoDAOTest {
+
+	@Test
+	public void testSalvar() {
+		
+		TipoContato t = new TipoContato();
+		
+		t.setDescricao("Teste Junit");
+		
+		TipoContatoDAO dao = new TipoContatoDAO();
+		
+		try {
+			dao.salvar(t);
+			fail("erro na gravação");
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+		
+	}
+
+}
