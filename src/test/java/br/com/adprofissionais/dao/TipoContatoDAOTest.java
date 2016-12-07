cd25499a8269981f40f1c5cfdf08b6ff51eaedb6
Luis Espoladore <laespol@sistema>
2016-12-07 20:23:19 -0200

    teste jenking 5

--- src/test/java/br/com/adprofissionais/dao/TipoContatoDAOTest.java
+++ src/test/java/br/com/adprofissionais/dao/TipoContatoDAOTest.java
@@ -1,6 +1,6 @@
 package br.com.adprofissionais.dao;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 import java.sql.SQLException;
 
@@ -12,22 +12,78 @@ public class TipoContatoDAOTest {
 
 	@Test
 	public void testSalvar() {
-		
+
 		TipoContato t = new TipoContato();
-		
+
 		t.setDescricao("Teste Junit");
-		
+
 		TipoContatoDAO dao = new TipoContatoDAO();
-		
+
 		try {
 			dao.salvar(t);
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			fail("Not yet implemented");
+			fail("Erro no momento de Salvar");
 		}
-		
+	}
+
+	@Test
+	public void testSalvarNull() {
+
+		TipoContato t = new TipoContato();
+
+		TipoContatoDAO dao = new TipoContatoDAO();
+
+		t.setDescricao(null);
+
+		try {
+			dao.salvar(t);
+			fail("Gravando registro nulo");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	public void testSalvarTamanhoSuperiorMaximo() {
+
+		TipoContato t = new TipoContato();
+
+		TipoContatoDAO dao = new TipoContatoDAO();
+
+		t.setDescricao("12345678901234567890123456789012345678901234567890");
+
+		try {
+			dao.salvar(t);
+			fail("Gravando registro com tamanho superior ao campo");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	public void testSalvarTamanhoMaximo() {
+
+		TipoContato t = new TipoContato();
+
+		TipoContatoDAO dao = new TipoContatoDAO();
+
+		t.setDescricao("123456789012345678901234567890123456789012345");
+
+		try {
+			dao.salvar(t);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("Gravando registro com tamanho superior ao campo");
+		}
+
 	}
 
 }
